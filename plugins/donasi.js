865c30cadfbca7a466f98cec432881d85bc47bbd
Defan11 <107857564+Defan11@users.noreply.github.com>
2022-07-19 19:11:50 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA : 081395861695
-├ DANA : 082221792667
+├ PULSA : 081321776496
+├ DANA : 081321776496
 └────
+>  _Ingin donasi? http://wa.me/6282135905896 Hasil donasi akan digunakan buat sewa atau beli *RDP/VPS* agar bot bisa berjalan 24jam tanpa kendala_
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
 handler.tags = ['info']
